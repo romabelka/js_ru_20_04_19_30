8c277ea6623ad9b84fa03b072247f0786a29da38
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-05-11 20:17:27 +0300

    use input

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import ArticleList from './ArticleList'
 import Chart from './Chart'
+import UserForm from './UserForm'
 
 class App extends Component {
     static propTypes = {
@@ -21,7 +22,8 @@ class App extends Component {
     render() {
         return (
             <div>
-                <a href="#" onClick = {this.updateCounter}>update chart</a>
+                <UserForm />
+                <a href = "#" onClick = {this.updateCounter}>update chart</a>
                 <ArticleList articles = {this.props.articles} />
                 <Chart articles = {this.props.articles} key={this.state.counter}/>
             </div>
