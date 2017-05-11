7fcf4da5859cbaf33237e70190e6d1d1bdd50b69
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-05-11 20:03:53 +0300

    use findDOMNode

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,20 +1,25 @@
 import React, {Component} from 'react'
+import {findDOMNode} from 'react-dom'
 import Article from './Article'
 import PropTypes from 'prop-types'
 import accordion from '../decorators/accordion'
 
 class ArticleList extends Component {
     componentDidMount() {
-        console.log('---', this)
+        const ref = this.refs[this.props.articles[0].id]
+        console.log('---', ref, findDOMNode(ref))
     }
+
 
     render() {
         const {articles, toggleOpenItem, isItemOpened} = this.props
 
         const elements = articles.map(article => <li key={article.id}>
-            <Article article={article}
-                     isOpen={isItemOpened(article.id)}
-                     toggleOpen={toggleOpenItem(article.id)}/>
+            <Article article = {article}
+                     isOpen = {isItemOpened(article.id)}
+                     toggleOpen = {toggleOpenItem(article.id)}
+                     ref = {article.id}
+            />
         </li>)
         return (
             <ul ref={this.getContainerRef}>
