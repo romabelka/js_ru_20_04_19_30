ce61549e72e360fce9cd4d836576854365a50544
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-05-22 20:10:22 +0300

    migrate to immutable

--- src/reducer/articles.js
+++ src/reducer/articles.js
@@ -1,23 +1,24 @@
 import {normalizedArticles as defaultArticles} from '../fixtures'
 import {DELETE_ARTICLE, ADD_COMMENT} from '../constants'
 import {arrayToMap} from '../utils'
+import {Map, Record} from 'immutable'
+
+const ArticleModel = Record({
+    id: null,
+    date: null,
+    title: null,
+    text: '',
+    comments: []
+})
 
 export default (articles = arrayToMap(defaultArticles), action) => {
     const {type, payload, randomId} = action
     switch (type) {
         case DELETE_ARTICLE:
-            const tmpArticles = articles
-            delete tmpArticles[payload.id]
-            return tmpArticles
+            return articles.delete(payload.id)
 
         case ADD_COMMENT:
-            return {
-                ...articles,
-                [payload.articleId]: {
-                    ...articles[payload.articleId],
-                    comments: (articles[payload.articleId].comments || []).concat(randomId)
-                }
-            }
+            return articles.updateIn([payload.articleId, 'comments'], (comments) => comments.concat(randomId))
     }
 
     return articles
