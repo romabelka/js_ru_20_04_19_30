42ef78d69f9f372c267d8f2d78caf10d2fb6d141
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-05-29 19:56:34 +0300

    add redirect

--- src/route_handlers/CommentsPage.js
+++ src/route_handlers/CommentsPage.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import CommentsPagination from '../components/CommentsPagination'
+import {Redirect, Route} from 'react-router-dom'
 
 function CommentsPage({match}) {
+    if (match.isExact) return <Redirect to='/comments/1'/>
+    return <Route path = '/comments/:id' render = {getCommentsPage}/>
+}
+
+function getCommentsPage({match}) {
     return <CommentsPagination page = {match.params.page}/>
 }
 
